8edccd37e76044cc6c0d2cfff2094bbbb670add0
victorvc3018 <114165356+victorvc3018@users.noreply.github.com>
2025-07-18 19:01:37 +0530

    Update ReplyIcon.tsx

--- components/icons/ReplyIcon.tsx
+++ components/icons/ReplyIcon.tsx
@@ -1,1 +1,25 @@
+import React from 'react';
 
+interface IconProps {
+  className?: string;
+}
+
+const ReplyIcon: React.FC<IconProps> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <polyline points="9 17 4 12 9 7"></polyline>
+    <path d="M20 18v-2a4 4 0 0 0-4-4H4"></path>
+  </svg>
+);
+
+export default ReplyIcon;
